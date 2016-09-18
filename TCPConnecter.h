7cc6b3d934240ba63c19d4688704e0c41e8e6920
Erik Nadel <erik.nadel@gmail.com>
2016-09-18 11:18:29 -0400

    Create TCPConnecter.h
    uploaded file from old project

---
+++ TCPConnecter.h
@@ -0,0 +1,16 @@
+#ifndef TCPCONNECTOR_H
+#define TCPCONNECTOR_H
+
+#include <netinet/in.h>
+#include "TCPStream.h"
+
+class TCPConnector
+{
+    public:
+        TCPStream* connect(const char* server, int port);
+    private:
+        int resolveHostName(const char* host, struct in_addr* addr);
+
+};
+
+#endif // TCPCONNECTOR_H
